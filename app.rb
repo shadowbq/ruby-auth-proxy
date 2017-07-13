d0a9213f578a338a70722df823c2fb5bce9bd463
shadowbq <shadowbq@gmail.com>
2017-07-13 15:26:38 -0400

    cleanup

--- app.rb
+++ app.rb
@@ -2,25 +2,8 @@ require 'bundler'
 Bundler.require
 
 # load the Database and User model
-require './model'
-
-Warden::Strategies.add(:password) do
-  def valid?
-    params['user'] && params['user']['username'] && params['user']['password']
-  end
-
-  def authenticate!
-    user = User.first(username: params['user']['username'])
-
-    if user.nil?
-      throw(:warden, message: "The username you entered does not exist.")
-    elsif user.authenticate(params['user']['password'])
-      success!(user)
-    else
-      throw(:warden, message: "The username and password combination is incorrect")
-    end
-  end
-end
+require './lib/model'
+require './lib/strategies'
 
 class SinatraAuthProxy < Sinatra::Base
   enable :sessions
